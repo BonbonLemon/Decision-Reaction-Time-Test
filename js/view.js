848be374e08d739d606475f48da5f163d3e9f289
Stephen Chen <chen.stephen.r@gmail.com>
2016-10-15 20:23:02 -0700

    can test many times

--- js/view.js
+++ js/view.js
@@ -36,8 +36,54 @@ View.prototype.start = function () {
 
     $('figure').append($pacman);
 
-    this.game.timeReaction(numRotations);
+    this.timeReaction(numRotations);
   }.bind(this), time);
+};
+
+View.prototype.timeReaction = function (numRotations) {
+  var startTime = Date.now();
+  window.onkeydown = function (e) {
+    if (e.keyCode >= 37 && e.keyCode <= 40) {
+      var endTime = Date.now();
+      var reactionTime = endTime - startTime;
+
+      var markSRC
+      if (e.keyCode - 37 == numRotations) {
+        markSRC = "./images/check_mark.png";
+      } else {
+        markSRC = "./images/x_mark.png";
+      }
+      var $mark = $('<img>');
+      $mark.attr({
+        src: markSRC,
+        class: "mark"
+      });
+
+      var $div = $('<div>');
+      $div.append($mark);
+      var $timeText = $('<span>');
+      $timeText.text(reactionTime + "ms");
+      $div.append($timeText);
+      $('figure').append($div);
+
+      var $h3 = $('<h3>');
+      $h3.text("Press space to try again")
+      $('figure').append($h3);
+      window.onkeydown = function (e) {
+        if (e.keyCode == 32) {
+          this.clearScreen();
+        }
+      }.bind(this);
+    }
+  }.bind(this);
+};
+
+View.prototype.clearScreen = function () {
+  $('div').remove();
+  $('img').remove();
+  $('h3').remove();
+
+  this.start();
 };
 
 module.exports = View;
